ed27e8a3f2672bbf80352151e786446f7123cefc
mattyjo6 <168124417+mattyjo6@users.noreply.github.com>
2024-04-30 17:23:38 -0400

    Update project.py

--- project.py
+++ project.py
@@ -103,7 +103,7 @@ def main():
     # [ST2] Button widget
     if st.sidebar.button("Show Pub"):
         filtered_df = filter_pubs_by_name(df, pub_name)
-        display_pubs(filtered_df)
+        
 
     # [ST3] Map widget
     st.subheader("Map of London Pubs")
